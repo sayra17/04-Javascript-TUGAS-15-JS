6f59ae59829a7703280b383ce26eae6d4434e92d
sayra17 <agussayra03@gmail.com>
2022-02-05 14:00:41 +0700

    Initial commit

---
+++ tugas15.js
@@ -0,0 +1,9 @@
+function pytagoran(x, y) {
+    var ab = (x)
+    console.log("Nilai AB adalah", ab);
+    var bc = (y)
+    console.log("Nilai BC adalah", bc)
+    var ac = Math.sqrt(Math.pow(ab, 2) + Math.pow(bc, 2));
+    console.log("Panjang sisi AC pada segitiga siku-siku tersebut adalah :", ac)
+}
+pytagoran(8,6) 
